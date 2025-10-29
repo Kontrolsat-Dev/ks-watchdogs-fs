28caf597f2dc9abe735279a5fd8f5143876cf660
rafawastaken <jrafaelp97@gmail.com>
2025-10-29 23:46:10 +0000

    add envs

--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -2,7 +2,7 @@ export const Endpoints = Object.freeze({
   // --------------------------------
   // ------------- BASE -------------
   // --------------------------------
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/v1",
+  BASE_URL: import.meta.env.VITE_API_BASE_URL,
   // --------------------------------
   // ------- System Endpoints -------
   // --------------------------------
